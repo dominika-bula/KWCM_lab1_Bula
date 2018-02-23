979524b87bd9fef1a8396b48a04ffa593aa25e03
Student <student@ib.polsl.pl>
2018-02-23 14:02:20 +0100

    Drugi commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "trygonometria.h"
+#include<iostream>
+
+int main()
+{
+
+std::cout<<degreemath::sin(90)<<std::endl;
+std::cout<<degreemath::cos(90)<<std::endl;
+std::cout<<degreemath::tan(90)<<std::endl;
+std::cout<<degreemath::ctg(90)<<std::endl;
+
+
+}
