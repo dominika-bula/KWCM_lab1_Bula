107fdf3bb6ddfe73d85753f8361dde5b9c446c22
Student <student@ib.polsl.pl>
2018-02-23 14:33:50 +0100

    Trzeci commit z wlaczona opcja USE_TRIGONOMETRY_DEGREE

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
+#include <LaborkaConfig.h>
+#ifdef  USE_TRIGONOMETRY_DEGREE
 #include "trygonometria.h"
+#else 
+#include<cmath>
+#endif
+
 #include<iostream>
 
 int main()
 {
+int result=0;
+double pi=3.14159;
 
-std::cout<<degreemath::sin(90)<<std::endl;
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+result = cos(45.0);
+#else
+result = cos(pi/4.0);
+#endif
+
+/*std::cout<<degreemath::sin(90)<<std::endl;
 std::cout<<degreemath::cos(90)<<std::endl;
 std::cout<<degreemath::tan(90)<<std::endl;
 std::cout<<degreemath::ctg(90)<<std::endl;
+*/
+std::cout<<result<<std::endl;
 
 
 }
